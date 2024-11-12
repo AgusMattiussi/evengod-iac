49196c230d64e134109ecc7c30165ed983d7bcba
jsasso <jsasso@itba.edu.ar>
2024-11-12 19:13:12 -0300

    Load user info after log in

--- resources/frontend/src/services/google_login.js
+++ resources/frontend/src/services/google_login.js
@@ -1,67 +1,88 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
+import { apiPost } from "./api";
+import { useSharedAuth } from "./auth";
+import { useNavigate } from "react-router-dom";
 
-const CLIENT_ID = "102897822622-j0m36vpo56fqetqb0sbf2k9rtv6tp9m7.apps.googleusercontent.com";
+const CLIENT_ID =
+    "102897822622-j0m36vpo56fqetqb0sbf2k9rtv6tp9m7.apps.googleusercontent.com";
 
 const GoogleSignIn = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
-    script.async = true;
-    script.onload = initializeGoogleSignIn;
-    document.body.appendChild(script);
+    const { setAccessToken } = useSharedAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://accounts.google.com/gsi/client";
+        script.async = true;
+        script.onload = initializeGoogleSignIn;
+        document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    const initializeGoogleSignIn = () => {
+        if (window.google) {
+            window.google.accounts.id.initialize({
+                client_id: CLIENT_ID,
+                callback: handleCredentialResponse,
+            });
+
+            window.google.accounts.id.renderButton(
+                document.getElementById("google-signin-button"),
+                { theme: "outline", size: "large" }
+            );
+        }
     };
-  }, []);
 
-  const initializeGoogleSignIn = () => {
-    if (window.google) {
-      window.google.accounts.id.initialize({
-        client_id: CLIENT_ID,
-        callback: handleCredentialResponse,
-      });
+    const handleCredentialResponse = async (response) => {
+        //console.log("Google Token Received:", response.credential);
 
-      window.google.accounts.id.renderButton(
-        document.getElementById("google-signin-button"),
-        { theme: "outline", size: "large" }
-      );
-    }
-  };
+        const googleToken = response.credential;
+        const decodedToken = jwtDecode(googleToken);
+        const { email } = decodedToken;
 
-  const handleCredentialResponse = async (response) => {
-    console.log("Google Token Received:", response.credential);
-  
-    const googleToken = response.credential;
-    const decodedToken = jwtDecode(googleToken);
-    const { email } = decodedToken;
-  
-    try {
-      const result = await fetch("https://vlf2ioj8o1.execute-api.us-east-1.amazonaws.com/prod/googleLogin", {
-        method: "POST",
-        body: JSON.stringify({ googleToken, email }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (result.ok) {
-        console.log("User linked successfully");
-      } else {
-        console.error("Error linking user", await result.json());
-      }
-    } catch (error) {
-      console.error("Error linking Google user:", error);
-    }
-  };
-  
+        try {
+            const body = JSON.stringify({ googleToken, email })
+            const response = await apiPost("/googleLogin", body);
+        
 
-  return (
-    <div>
-      <div id="google-signin-button"></div>
-    </div>
-  );
+            if (response.data) {
+                console.log("User linked successfully");
+                //console.log("Result: ", response);
+                //console.log("Result body: ", response.data);
+
+                localStorage.setItem(
+                    "accessToken",
+                    response.data.AccessToken || ""
+                );
+                localStorage.setItem(
+                    "refreshToken",
+                    response.data.RefreshToken || ""
+                );
+                localStorage.setItem("idToken", response.data.IdToken || "");
+                localStorage.setItem(
+                    "sub",
+                    jwtDecode(response.data.AccessToken).sub || ""
+                );
+            
+                await setAccessToken(response);
+                navigate("/");
+
+            } else {
+                console.error("Error linking user");
+            }
+        } catch (error) {
+            console.error("Error linking Google user:", error);
+        }
+    };
+
+    return (
+        <div>
+            <div id="google-signin-button"></div>
+        </div>
+    );
 };
 
 export default GoogleSignIn;
